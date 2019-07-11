bc35cba1d57bbd79922015ebb681e4011618832e
zack <zackaminu@bitbucket.com>
2019-07-11 08:59:44 +0100

    remove duplicate goos receipt detail from localstorage

--- www/app/views/goodreciept/goodreciept-controller.js
+++ www/app/views/goodreciept/goodreciept-controller.js
@@ -15,7 +15,6 @@
 
     vm.showDetail = function (receipt) {
       $rootScope.goodReceipt = receipt;
-      console.log($rootScope.goodReceipt);
       $state.go('main.goodreciept-new');
     }
 
@@ -25,7 +24,9 @@
 
     var vm = this;
     vm.task = sharedSvc.getStorage('UserTask');
-    
+
+    var userTaskRepository = sharedSvc.initialize('api/userjob/' + sharedSvc.getStorage("UserID"));
+
     vm.submit = function () {
       swal({
         type: 'warning',
@@ -38,6 +39,7 @@
         closeOnConfirm: true,
         closeOnCancel: true
       }).then(function () {
+        submitTasks();
         $state.go('main.success')
       }, function () {
         //$state.go('index.dashboard') 
@@ -63,7 +65,7 @@
       });
     };
 
-    vm.deleteTask = function () {
+    vm.deleteTask = function (lotNo) {
       swal({
         type: 'error',
         text: 'Are you sure you want to delete this task?',
@@ -75,7 +77,14 @@
         closeOnConfirm: true,
         closeOnCancel: true
       }).then(function () {
-        toastr.success("Task deleted successfully")
+        if (lotNo !== undefined || lotNo !== null) {
+          let tasks = vm.task.tasks.filter((x) => x.lotNo !== lotNo);
+
+          vm.task.tasks = tasks;
+          sharedSvc.createStorageParam("UserTask", vm.task);
+          toastr.success("Task deleted successfully");
+        }
+
         return;
       }, function () {
         return;
@@ -83,11 +92,26 @@
     };
 
 
+    function submitTasks() {
+      let data = sharedSvc.getStorage("UserTask")
+      data.PalletDetailModel = data.tasks;
+
+      userTaskRepository.save({}, data, function (response) {
+        vm.isBusy = false;
+        vm.isBusy2 = false;
+        vm.formData = {};
+        toastr.success(response.message);
+        // $state.go("inventory.list-stock-inward");
+      }, function (error) {
+        vm.isBusy = false;
+        vm.isBusy2 = false;
+      })
+    }
+
   }]);
 
   angular.module("app").controller("goodRecieptNewCtrl", ["sharedSvc", "$state", '$scope', "$rootScope", "toastr", "barcodeService", function (sharedSvc, $state, $scope, $rootScope, toastr, barcodeService) {
     var vm = this;
-
     vm.formData = {};
     vm.products = [];
     vm.batches = [];
@@ -105,7 +129,6 @@
 
     stockStateRepository.get(function (response) {
       vm.stockStates = response.result;
-      console.log(vm.stockStates);
     });
 
 
@@ -132,16 +155,35 @@
 
     vm.save = function (data) {
       const task = sharedSvc.getStorage('UserTask');
-
+      console.log("data", data);
       if (Object.keys(vm.formData).length === 0) {
         toastr.error("You cannot save an empty task.");
         return;
       }
 
+      //get goood receipt note detail from which the pallet comes from
+      let noteDetail = vm.goodReceipt.GoodReceiveNoteDetails.find((x) => {
+        return x.ProductID === vm.formData.productID && x.BatchID === vm.formData.batchID
+          && x.ReceivedQtyMeasurementUnit === vm.formData.receivedQtyMeasurementUnit
+      })
+
+      vm.formData = {
+        ...vm.formData,
+        detailID: noteDetail.ID,
+        parentID: noteDetail.GoodReceiveNoteID,
+        status: "Pending",
+        palletteNo: vm.formData.lotNo,
+        donorID: noteDetail.DonoID,
+        serialNoEnd: noteDetail.SerialNoEnd,
+        serialNoStart: noteDetail.SerialNoStart
+      };
+
       if (task === null || task === undefined) { // user does not have a task already
         sharedSvc.createStorageParam('UserTask', {
           tasks: [vm.formData],
-          documentNo: vm.goodReceipt.DocumentNo
+          documentNo: vm.goodReceipt.DocumentNo,
+          status: "pending",
+          userID: sharedSvc.getStorage("UserID")
         });
       } else {
         if (task.tasks.length > 0) {
@@ -149,7 +191,9 @@
 
           sharedSvc.createStorageParam('UserTask', {
             tasks: [...taskList, vm.formData],
-            documentNo: vm.goodReceipt.DocumentNo
+            documentNo: vm.goodReceipt.DocumentNo,
+            status: "pending",
+            userID: sharedSvc.getStorage("UserID")
           });
         }
       }
@@ -167,7 +211,7 @@
         receipt.GoodReceiveNoteDetails.map((prod) => {
           product = {
             ProductID: prod.ProductID,
-            ProductName: prod.ProductID,
+            ProductName: prod.ProductName,
             ProductUniqueID: prod.ProductUniqueID
           }
           vm.products.push(product);
@@ -196,9 +240,39 @@
             vm.productMeasures.push(measure);
           }
         })
-
-        console.log(vm.products, vm.batches, vm.productMeasures, vm.stockStates);
       }
     }
+
+    vm.setSelectedStockState = function (item) {
+      if (item !== undefined || item !== null) {
+        vm.formData.stockStateName = item.StockStateCode;
+        vm.formData.stockStateID = item.ID;
+        vm.formData.stockState = item.StockState;
+      }
+    }
+
+    vm.setSelectedBatch = function (item) {
+      if (item !== undefined || item !== null) {
+        vm.formData.batchID = item.BatchID;
+        vm.formData.batchExpiringDate = item.BatchExpiringDate;
+        vm.formData.batchManufaturingDate = item.BatchManufaturingDate;
+      }
+    }
+
+    vm.setSelectedMeasure = function (item) {
+      if (item !== undefined || item !== null) {
+        vm.formData.measurementID = item.MeasurementID;
+        vm.formData.receivedQtyMeasurementUnit = item.ReceivedQtyMeasurementUnit;
+      }
+    }
+
+    vm.setSelectedProduct = function (item) {
+      if (item !== undefined || item !== null) {
+        vm.formData.productID = item.ProductID;
+        vm.formData.productName = item.ProductName;
+        vm.formData.productUniqueID = item.ProductUniqueID;
+      }
+    }
+
   }]);
 })()
